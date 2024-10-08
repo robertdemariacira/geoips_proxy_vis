359e179d38ec240f9783a81db4a8d3bc1e400f3d
Robert DeMaria <robert.demaria@colostate.edu>
2024-10-08 14:27:38 -0600

    Added meteosat 9 test script.

--- tests/scripts/get_test_data.sh
+++ tests/scripts/get_test_data.sh
@@ -2,10 +2,20 @@
 ahi_subdir=pvis_test_data/ahi/20241001/2200/
 ahi_tar_filename=ahi_2024-10-01T22.tar
 
+m9_subdir=pvis_test_data/m9/20240519/1100/
+m9_tar_filename=m9_2024-05-19T11.tar
+
 cd $GEOIPS_TESTDATA_DIR
 mkdir -p $ahi_subdir
 pushd $ahi_subdir
 wget https://irma.cira.colostate.edu/index.php/s/R7iEXHwCxStE3Mi/download/$ahi_tar_filename
 tar -xf $ahi_tar_filename
 rm $ahi_tar_filename
+popd
+
+mkdir -p $m9_subdir
+pushd $m9_subdir
+wget https://irma.cira.colostate.edu/index.php/s/2srsFL49D3j5NRM/download/$m9_tar_filename
+tar -xf $m9_tar_filename
+rm $m9_tar_filename
 popd
--- tests/scripts/m9.pvis-Fully-Specified.sh
+++ tests/scripts/m9.pvis-Fully-Specified.sh
@@ -12,24 +12,133 @@
 
 #!/bin/bash
 
-data_subdir=test_data_noaa_aws/data/goes16/20200918/1950
+data_subdir=pvis_test_data/m9/20240519/1100
 
 run_procflow \
-    $GEOIPS_TESTDATA_DIR/$data_subdir/OR_ABI-L1b-RadF-M6C02_G16_s20202621950205_e20202621959513_c20202621959546.nc \
-    $GEOIPS_TESTDATA_DIR/$data_subdir/OR_ABI-L1b-RadF-M6C07_G16_s20202621950205_e20202621959524_c20202621959577.nc \
-    $GEOIPS_TESTDATA_DIR/$data_subdir/OR_ABI-L1b-RadF-M6C11_G16_s20202621950205_e20202621959513_c20202621959583.nc \
-    $GEOIPS_TESTDATA_DIR/$data_subdir/OR_ABI-L1b-RadF-M6C13_G16_s20202621950205_e20202621959525_c20202622000005.nc \
-    $GEOIPS_TESTDATA_DIR/$data_subdir/OR_ABI-L1b-RadF-M6C15_G16_s20202621950205_e20202621959518_c20202621959594.nc \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-_________-EPI______-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000008___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000009___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000010___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000011___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000012___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000013___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000014___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000015___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000016___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000017___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000018___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000019___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000020___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000021___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000022___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000023___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-HRV______-000024___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_016___-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_016___-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_016___-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_016___-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_016___-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_016___-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_016___-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_016___-000008___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_039___-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_039___-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_039___-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_039___-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_039___-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_039___-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_039___-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_039___-000008___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_087___-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_087___-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_087___-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_087___-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_087___-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_087___-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_087___-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_087___-000008___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_097___-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_097___-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_097___-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_097___-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_097___-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_097___-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_097___-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_097___-000008___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_108___-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_108___-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_108___-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_108___-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_108___-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_108___-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_108___-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_108___-000008___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_120___-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_120___-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_120___-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_120___-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_120___-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_120___-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_120___-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_120___-000008___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_134___-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_134___-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_134___-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_134___-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_134___-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_134___-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_134___-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-IR_134___-000008___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-_________-PRO______-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS006___-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS006___-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS006___-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS006___-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS006___-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS006___-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS006___-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS006___-000008___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS008___-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS008___-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS008___-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS008___-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS008___-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS008___-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS008___-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-VIS008___-000008___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_062___-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_062___-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_062___-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_062___-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_062___-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_062___-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_062___-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_062___-000008___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_073___-000001___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_073___-000002___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_073___-000003___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_073___-000004___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_073___-000005___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_073___-000006___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_073___-000007___-202405191100-__ \
+    $GEOIPS_TESTDATA_DIR/$data_subdir/H-000-MSG2__-MSG2_IODC___-WV_073___-000008___-202405191100-__ \
     --procflow single_source \
     -l debug \
     --reader_name generic_satpy \
     --product_name m9_geo_proxy_vis \
     --output_formatter satpy_tiles_formatter \
-    --output_formatter_kwargs '{"config_path": "tile_configs/m9_proxy_vis_tile_template.yaml", "xarray_var_name":"proxy_vis", "output_var_name":"proxy_vis", "output_path":"/home/rdemaria/projects/2024/geoips_proxy_vis/tests/outputs/m9_tiles/", "environment_prefix":"OR", "source_name":"CIRA", "sector_id":"EFD", "tile_size":[2200, 2750], "satellite_altitude_attrib_name":"satellite_nominal_altitude", "fake_channel":2, "fill_value":-1}' \
+    --output_formatter_kwargs '{"config_path": "tile_configs/m9_proxy_vis_tile_template.yaml", "xarray_var_name":"proxy_vis", "output_var_name":"proxy_vis", "output_path":"/home/rdemaria/projects/2024/geoips_proxy_vis/tests/outputs/m9_tiles/", "environment_prefix":"OR", "source_name":"CIRA", "sector_id":"M9", "tile_size":[464, 464], "fake_channel":1}' \
     --filename_formatter geoips_fname \
     --minimum_coverage 0 \
     --no_presectoring \
-    --reader_kwargs '{"force_compute": true, "satpy_reader": "abi_l1b", "channel_groups":{"0.5km":["C02"], "2km":["C07", "C11", "C13", "C15"]}, "source_name":"abi", "platform_name":"goes16"}'
+    --reader_kwargs '{"force_compute": true, "satpy_reader": "seviri_l1b_hrit", "channel_groups":{"vis":["VIS006"], "ir":["IR_039", "IR_087", "IR_108", "IR_120"]}, "source_name":"meteosat9", "platform_name":"meteosat9", "load_kwargs":{"upper_right_corner":"NE"}}'
 ss_retval=$?
 
 exit $((ss_retval))
